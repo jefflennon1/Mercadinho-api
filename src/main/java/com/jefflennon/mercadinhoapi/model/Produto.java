cb6319f01c6aefa670fb3c21b7580b94794ba77f
jefflenno1 <jeffersonlennon1@outlook.com>
2023-08-13 12:14:43 -0300

    Iniciando a modelagem: Começando a definir os models (ainda de forma bem simples)

---
+++ src/main/java/com/jefflennon/mercadinhoapi/model/Produto.java
@@ -0,0 +1,46 @@
+package com.jefflennon.mercadinhoapi.model;
+
+public class Produto {
+	
+	private Long id;
+	
+	private String nome;
+	private double preco;
+	private Categoria categoria;
+	private int quantidadeDisponivel;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	public int getQuantidadeDisponivel() {
+		return quantidadeDisponivel;
+	}
+	public void setQuantidadeDisponivel(int quantidadeDisponivel) {
+		this.quantidadeDisponivel = quantidadeDisponivel;
+	}
+	
+	
+	
+}
