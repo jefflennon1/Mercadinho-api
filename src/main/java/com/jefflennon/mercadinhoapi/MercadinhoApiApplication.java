5f013c806a77a7cef41b138ae4e1f8236bf74d3f
jefflenno1 <jeffersonlennon1@outlook.com>
2023-08-13 11:14:20 -0300

    criando o projeto e configurando a concexão com o banco de dados postgres

---
+++ src/main/java/com/jefflennon/mercadinhoapi/MercadinhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.jefflennon.mercadinhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadinhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadinhoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefflennon/mercadinhoapi/MercadinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefflennon.mercadinhoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
